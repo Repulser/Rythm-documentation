f3d3f9f13cd1a585b0e7880178f4262594e462db
HmmOrange <51955417+HmmOrange@users.noreply.github.com>
2020-11-07 16:29:59 +0700

    Rename misc_help -> get_support

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'Getting Started': ['start', 'adding_rythm', 'play_song', 'faq'],
     'Knowledge Center': ['features', 'commands', 'permissions', 'settings'],
-    Troubleshooting: ['basic_troubleshooting', 'audio_troubleshooting', 'mobile_troubleshooting', 'misc_help'],
+    'Troubleshooting': ['basic_troubleshooting', 'audio_troubleshooting', 'mobile_troubleshooting', 'get_support'],
   },
 };
